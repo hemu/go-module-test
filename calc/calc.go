e434a12bad4f6eee8a985809d253527a377902ee
Hemu Arumugam <hemu@uber.com>
2020-02-17 20:27:41 -0800

    accept variable args calc.Add

--- calc/calc.go
+++ calc/calc.go
@@ -1,7 +1,11 @@
 package calc
 
-func Add(a int, b int) int {
-  return a + b
+func Add(nums ...int) int {
+  sum := 0
+  for _, num := range nums {
+    sum += num
+  }
+  return sum
 }
 
 func Sub(a int, b int) int {
